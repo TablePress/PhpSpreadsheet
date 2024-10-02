8e491e0eb7ed5c14e34333deb446b4f3afadeb6d
TobiasBg <git@tobias.baethge.com>
2024-10-02 21:13:53 +0200

    Revert "Drop PHP 7.4 support"
    This reverts commit f131ca30e66f659a761aab17ffdc5e6f869e328c.
    
    Only PHP files in src/ and composer.json are reverted.

--- src/PhpSpreadsheet/Style/NumberFormat/Wizard/Accounting.php
+++ src/PhpSpreadsheet/Style/NumberFormat/Wizard/Accounting.php
@@ -16,6 +16,12 @@ class Accounting extends CurrencyBase
      */
     protected function getLocaleFormat(): string
     {
+        if (version_compare(PHP_VERSION, '7.4.1', '<')) {
+            // @codeCoverageIgnoreStart
+            throw new Exception('The Intl extension does not support Accounting Formats below PHP 7.4.1');
+            // @codeCoverageIgnoreEnd
+        }
+
         if (self::icuVersion() < 53.0) {
             // @codeCoverageIgnoreStart
             throw new Exception('The Intl extension does not support Accounting Formats without ICU 53');
