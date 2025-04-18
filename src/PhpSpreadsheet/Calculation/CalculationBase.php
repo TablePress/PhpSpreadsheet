520c2e59a28788b692a7cb928f9016418e72a709
TobiasBg <git@tobias.baethge.com>
2025-04-18 21:06:44 +0200

    Adjustment for TablePress: Add aliases and custom functions that TablePress uses.
    These functions don't exist in Excel and need to be aliases or replaced by a custom implementation, to maintain backwards compatibility.
    These functions are deprecated in TablePress and should be replaced with their corresponding function!
    For functions with an underscore (_) in their name, the regexp is adjusted.
    
    This moves commit https://github.com/TablePress/TablePress-Premium/commit/72f686e15f8ef6c533c99084f67a0511ec4a4124 to the custom TablePress fork of PHPSpreadsheet that is loaded via Composer.

--- src/PhpSpreadsheet/Calculation/CalculationBase.php
+++ src/PhpSpreadsheet/Calculation/CalculationBase.php
@@ -69,4 +69,48 @@ class CalculationBase
 
         return false;
     }
+
+    /**
+     * Registers custom functions and aliases that TablePress uses.
+     *
+     * These functions don't exist in Excel and need to be aliases or replaced by a custom implementation, to maintain backward compatibility.
+     * These functions are deprecated in TablePress and should be replaced with their corresponding function!
+     *
+     * For functions with an underscore (_) in their name, the Calculation::CALCULATION_REGEXP_FUNCTION regexp has been adjusted.
+     */
+    protected function register_tablepress_aliases_and_custom_functions() {
+        // Trigonometric ARC functions only have an A prefix in Excel.
+        FunctionArray::$phpSpreadsheetFunctions['ARCCOS']  = FunctionArray::$phpSpreadsheetFunctions['ACOS'];
+        FunctionArray::$phpSpreadsheetFunctions['ARCCOSH'] = FunctionArray::$phpSpreadsheetFunctions['ACOSH'];
+        FunctionArray::$phpSpreadsheetFunctions['ARCCOT']  = FunctionArray::$phpSpreadsheetFunctions['ACOT'];
+        FunctionArray::$phpSpreadsheetFunctions['ARCCOTH'] = FunctionArray::$phpSpreadsheetFunctions['ACOTH'];
+        FunctionArray::$phpSpreadsheetFunctions['ARCSIN']  = FunctionArray::$phpSpreadsheetFunctions['ASIN'];
+        FunctionArray::$phpSpreadsheetFunctions['ARCSINH'] = FunctionArray::$phpSpreadsheetFunctions['ASINH'];
+        FunctionArray::$phpSpreadsheetFunctions['ARCTAN']  = FunctionArray::$phpSpreadsheetFunctions['ATAN'];
+        FunctionArray::$phpSpreadsheetFunctions['ARCTAN2'] = FunctionArray::$phpSpreadsheetFunctions['ATAN2'];
+        FunctionArray::$phpSpreadsheetFunctions['ARCTANH'] = FunctionArray::$phpSpreadsheetFunctions['ATANH'];
+
+        // Aliases for functions with different names in Excel.
+        FunctionArray::$phpSpreadsheetFunctions['MEAN']       = FunctionArray::$phpSpreadsheetFunctions['AVERAGE'];
+        FunctionArray::$phpSpreadsheetFunctions['CEIL']       = FunctionArray::$phpSpreadsheetFunctions['CEILING'];
+        FunctionArray::$phpSpreadsheetFunctions['RAND_INT']   = FunctionArray::$phpSpreadsheetFunctions['RANDBETWEEN'];
+        FunctionArray::$phpSpreadsheetFunctions['RAND_FLOAT'] = FunctionArray::$phpSpreadsheetFunctions['RAND'];
+
+        // Custom functions for which there is no corresponding function in Excel.
+        FunctionArray::$phpSpreadsheetFunctions['NUMBER_FORMAT'] = array(
+            'category'      => Category::CATEGORY_TEXT_AND_DATA,
+            'functionCall'  => [TextData\Format::class, 'NUMBER_FORMAT'],
+            'argumentCount' => '1,2',
+        );
+        FunctionArray::$phpSpreadsheetFunctions['NUMBER_FORMAT_EU'] = array(
+            'category'      => Category::CATEGORY_TEXT_AND_DATA,
+            'functionCall'  => [TextData\Format::class, 'NUMBER_FORMAT_EU'],
+            'argumentCount' => '1,2',
+        );
+        FunctionArray::$phpSpreadsheetFunctions['RANGE'] = array(
+            'category'      => Category::CATEGORY_STATISTICAL,
+            'functionCall' => [Statistical\Range::class, 'RANGE'],
+            'argumentCount' => '1+',
+        );
+    }
 }
